44aae9a88b29669cda15f7be0c0aa432392141db
Nate <github@nategramer.com>
2022-12-28 12:59:28 -0600

    Add ingress gateway & fmt

--- istio.tf
+++ istio.tf
@@ -1,19 +1,31 @@
 resource "helm_release" "istio-base" {
-  name = "istio-base"
-  namespace = "istio-system"
-  chart = "base"
-  version = "1.16.1"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  create_namespace = true
+  name              = "istio-base"
+  namespace         = "istio-system"
+  chart             = "base"
+  version           = "1.16.1"
+  repository        = "https://istio-release.storage.googleapis.com/charts"
+  create_namespace  = true
   dependency_update = true
 }
 
 resource "helm_release" "istiod" {
-  name = "istiod"
-  namespace = "istio-system"
-  chart = "istiod"
-  version = "1.16.1"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  create_namespace = true
+  depends_on        = [helm_release.istio-base]
+  name              = "istiod"
+  namespace         = "istio-system"
+  chart             = "istiod"
+  version           = "1.16.1"
+  repository        = "https://istio-release.storage.googleapis.com/charts"
+  create_namespace  = true
+  dependency_update = true
+}
+
+resource "helm_release" "istio-ingressgateway" {
+  depends_on        = [helm_release.istiod]
+  name              = "istio-ingressgateway"
+  namespace         = "istio-ingressgateway"
+  chart             = "gateways"
+  version           = "1.16.1"
+  repository        = "https://istio-release.storage.googleapis.com/charts"
+  create_namespace  = true
   dependency_update = true
 }
