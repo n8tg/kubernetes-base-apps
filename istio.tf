b5137e6399404767587fd7aeff8637202d7430ce
Nate <github@nategramer.com>
2022-12-29 12:31:13 -0600

    update istio version

--- istio.tf
+++ istio.tf
@@ -2,7 +2,7 @@ resource "helm_release" "istio-base" {
   name              = "istio-base"
   namespace         = "istio-system"
   chart             = "istio"
-  version           = "0.1.1"
+  version           = "0.1.2"
   repository        = "https://helm.nategramer.com"
   create_namespace  = true
   dependency_update = true
